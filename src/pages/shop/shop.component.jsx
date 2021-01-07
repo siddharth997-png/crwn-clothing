1693cbc9d2f98e3585a027466bee499b0c609710
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-07 16:08:13 +0530

    added collections preview page and connected it from shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import CollectionsPreview from '../../components/collection-preview/collection-preview.component';
 import SHOP_DATA from './shop.data.js';
 
 import './shop.styles.scss';
@@ -13,8 +15,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return(
-      <div>SHOP PAGE</div>
+    const {collections} = this.state;
+    return(<div className='shop-page'>
+      {
+        collections.map( ({id, ...otherProps}) => (
+          <CollectionsPreview key={id} {...otherProps} />
+        ))
+      }
+      </div>
     )
   }
 }
