cf12b65a13b4dad7dfc9a1aaa33fdb6618796de8
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-11 13:05:34 +0530

    added styles for google button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import {auth} from '../../firebase/firebase.utils';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'; 
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className='header'>
     <div className='logo-container'>
       <Link className='logo' to='/'>
@@ -14,7 +16,13 @@ const Header = () => (
     </div>
     <div className='options'>
       <Link className='option' to='/shop'>SHOP</Link>
-      <Link className='option' to='/contact'>CONTACT</Link>
+      <Link className='option' to='/contact'>CONTACT</Link> 
+      {
+        ( currentUser ? 
+          (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>) :
+          (<Link className='option' to='/signin'>SIGN IN</Link>)
+        )
+      } 
     </div>
   </div>
 )
