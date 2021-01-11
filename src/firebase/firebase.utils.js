6debcb1271fd2ed4a91c01a079ca2ed981c3242d
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-11 12:09:01 +0530

    added firebase setup inside firebase.utils.js

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyB0sbTjsUQAjfbBOdyv8DoYEIwVJpj7-C4",
+  authDomain: "crwn-clothing-6abc7.firebaseapp.com",
+  projectId: "crwn-clothing-6abc7",
+  storageBucket: "crwn-clothing-6abc7.appspot.com",
+  messagingSenderId: "853342731974",
+  appId: "1:853342731974:web:ee6a834c364f4a8f62a1c7",
+  measurementId: "G-DET7C7D8QY"
+};
+
+firebase.initializeApp(config);
