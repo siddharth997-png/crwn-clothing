7e85438520a0ef32dcb67a3606633282993e365f
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-11 12:37:31 +0530

    added google sign in button in sign in component and in converted app.js to class component so that we have access to user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
+import React from 'react';
+
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
-function App() {
-  return (
-    <div>
-      <Route path='/' component={Header} />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser : null
+    }
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser : user });
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path='/' component={Header} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
