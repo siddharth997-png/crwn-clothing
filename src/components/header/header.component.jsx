86d4b3afc2857123aca9d2bfbc74d5b33fee1848
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-14 01:21:20 +0530

    added toggleCartHidden functionality by creating cart reducer mapDispatchToProps in cart-icon component and mapStateToProps in header component to condinationally render cartDropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <div className='logo-container'>
       <Link className='logo' to='/'>
@@ -28,12 +28,15 @@ const Header = ({currentUser}) => (
       } 
       <CartIcon/>
     </div>
-    <CartDropdown />
+    {
+      hidden ? null : <CartDropdown />
+    }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser : state.user.currentUser
+const mapStateToProps = ({user : {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,6 @@
+const CartActionTypes = {
+  TOGGLE_CART_HIDDEN : 'TOGGLE_CART-HIDDEN'
+}
+//this only contains an object mapping to strings
+
+export default CartActionTypes;
