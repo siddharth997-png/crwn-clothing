6413a88f31db4e05e2657bd499ce9b9f55d7ef8c
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-15 12:05:53 +0530

    added styles in custom button and collection item component to show add to cart button in collection item component, STARTING CART ITEMS FEATURE IN REDUX

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component'
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({id, name, imageUrl, price}) => (
@@ -14,6 +16,7 @@ const CollectionItem = ({id, name, imageUrl, price}) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton className='custom-button' inverted> Add to Cart </CustomButton>
   </div>
 )
 
