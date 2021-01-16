d0b967eb9d04a62f8786fcc60a3999b7894fb674
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-16 13:17:31 +0530

    added reselect for items count and cartItems, it is used for writing resuable mapStateToProps code and for saving rerenders

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,23 +3,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemsCount }) => (
   <div className='cart-icon'>
     <ShoppingIcon className='shopping-icon' onClick={() => toggleCartHidden()}/>
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemsCount}</span>
   </div>
 )
+
+const mapStateToProps = state => ({
+  itemsCount : selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden : () => dispatch(toggleCartHidden())
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps)
   (CartIcon);
