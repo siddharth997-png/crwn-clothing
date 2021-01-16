c6fb740a7439a808840baa970fbf6ff5defd20da
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-16 13:59:39 +0530

    added checkout page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -50,6 +51,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
+          
           <Route exact path='/signin' 
           render={() => 
           (this.props.currentUser ? 
