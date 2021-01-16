3114fd10a444ae525a9a7a007f9f50748c652d4b
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-16 15:35:19 +0530

    used dispatch shorthand in cart dropdown component, going to build checkout-item component next

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,13 @@ export const selectCartItems = createSelector(
   (cart) => cart.cartItems
 );
 
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce(
+    (a,item) => a+parseFloat(item.price)*parseInt(item.quantity)
+  ,0 )
+)
+
 export const selectCartHidden = createSelector(
   [selectCart],
   (cart) => cart.hidden
