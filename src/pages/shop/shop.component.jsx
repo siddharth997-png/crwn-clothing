e14b0b5f74a6a3bc01741bc0a909546bd7421103
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-01-17 22:34:06 +0530

    added collections overview component so that shop page can have advanced routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import { shopCollectionsSelector } from '../../redux/shop/shop.selectors';
-
-import CollectionsPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 import './shop.styles.scss';
 
-const ShopPage = ({ collections }) =>(
+const ShopPage = () =>(
   <div className='shop-page'>
-  {
-    collections.map( ({id, ...otherProps}) => (
-      <CollectionsPreview key={id} {...otherProps} />
-    ))
-  }
+    <CollectionsOverview/>
   </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections: shopCollectionsSelector
-})
-  
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
